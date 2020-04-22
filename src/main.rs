0583bb67aeb50ea7776a8b0a3c521caeb8bcd4e7
Andre Sammut <volfym@surfer-172-29-6-21-hotspot.internet-for-guests.com>
2020-04-22 15:13:01 +0200

    added quit / skip options, switched random with shuffle

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
 // refer to https://github.com/schulke-214/connect-four for help
 // flashcard program requirements:
 // - Simple terminal gui
-// * Vocabulary load Done! 
 // - - Improve CSVs (once other stuff is sorted out)
 // - - allow loading custom csv
-// - - Figure out how to autogenerate menu options? is this necessary?
+// - "youre close" message for typos
+// * Vocabulary load Done! 
+// * * Figure out how to autogenerate menu options? is this necessary?
 // * app shows random word from dictionary
-// - - avoid repetitions
+// * * avoid repetitions
 // * player has to input word (error check)
 // * * input functions need to be combined
 // * * compare input with word match from dict
-// * - if wrong allow another input or allow player to go to next card
+// * * if wrong allow another input or allow player to go to next card
 // * * if right show correct and show next card
-// - allow player to quit
-// - "youre close" message for typos
+// * allow player to quit
 
 use csv::{self, Reader};
 use std::{path::Path,io};
-use rand::{thread_rng, Rng};
+use rand::{thread_rng, seq::SliceRandom};
 
+// defines a language to make loading info easier
 struct Language {
     lang_one: String,
     lang_two: String,
@@ -33,6 +34,9 @@ impl Language {
             selection,
             dicti,
         }
+    }
+    fn generate_menu_option(&self) -> String {
+        format!("\nTo load {}-{} cards input {}.", self.lang_one, self.lang_two, self.selection)
     }
 }
 
@@ -56,21 +60,24 @@ fn load_csv(p: &Path) -> Vec<(String, String)> {
     loaded_dict
 }
 
-fn load_languages() -> Vec<(String, String)> {
+// defines languages, asks player for choice, returns the loaded dictionary
+fn load_language() -> Vec<(String, String)> {
     let eng_ger = Language::new(
         "English".to_string(), 
         "German".to_string(), 
         1, 
         load_csv(Path::new("voc/eng_ger.csv")));
-    let eng_ger = Language::new(
+    let eng_mlt = Language::new(
         "English".to_string(), 
         "Maltese".to_string(), 
         2, 
         load_csv(Path::new("voc/eng_mlt.csv")));
+    
+    println!("{}{}\n", 
+        eng_ger.generate_menu_option(), 
+        eng_mlt.generate_menu_option());
 
-    println!("\nTo load {}-{} cards input {}.", eng_ger.lang_one, eng_ger.lang_two, eng_ger.selection);
-    println!("To load {}-{} cards input {}.\n", eng_mlt.lang_one, eng_mlt.lang_two, eng_mlt.selection);
-
+    // find a way to use the Language struct selection field as a pattern?
     loop{
         match user_input().parse().unwrap_or(0){ 
             1 => return eng_ger.dicti,
@@ -83,7 +90,7 @@ fn load_languages() -> Vec<(String, String)> {
     };
 }
 
-
+// loads input for choice/word
 fn user_input() -> String {
     let mut input = String::new();
     // take user input
@@ -91,34 +98,53 @@ fn user_input() -> String {
     // clean newline \n input
     input.split_off(input.len() -1);
 
+    if input == ":q" {
+        std::process::exit(0);
+    }
+
     input
 }
 
 ////////////////////////////////////////////////////////////
 
 fn main() {
-    // to keep clutter out of main, but could be done in main
-    let loaded_dict = load_languages();
+    
+    println!("Welcome to my Flashcard program! (:q to quit at any time)");
+
+    let mut loaded_dict = load_language();
+
+    println!("How many words would you like to practice today?");
+    
+    let words_to_practice = user_input().parse().unwrap_or(0);
 
     println!("{:?}", loaded_dict);
     // rng
     let mut rng = thread_rng();
+    // shuffles dictionary randomly
+    loaded_dict.shuffle(&mut rng);
     
     // loop main function until necessary
-    for _i in 0..10 {
-        // random number in range of dict length
-        let rdm = rng.gen_range(0, loaded_dict.len());
-        println!("What is the translation for: {}", loaded_dict[rdm].0);
+    for i in 0..words_to_practice {
+        
+        // allows wrap around
+        let x = i % loaded_dict.len();
+        // keeps things random
+        if i % loaded_dict.len() == 0 {loaded_dict.shuffle(&mut rng);}
+        println!("What is the translation for: {} (:n to skip)", loaded_dict[x].0);
 
         loop {
             // load input function and compare
             // if correct go to next word, if wrong allow player to try again
-            if user_input().to_lowercase() == loaded_dict[rdm].1.to_lowercase() {
-                println!("Good job!");
+            let input = user_input().to_lowercase();
+            if  input == loaded_dict[x].1.to_lowercase() {
+                println!("Good job!\n");
                 break;
-            } else { 
+            } else if input != ":n" { 
                 println!("Try again.");
                 continue;
+            } else {
+                println!("Correct answer is {}.\n", loaded_dict[x].1);
+                break;
             }
         }
     }
